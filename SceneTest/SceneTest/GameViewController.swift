f20b1e683278fa591fb75cedcfe00eff499ba342
Chen Wei <Millionaryearl@users.noreply.github.com>
2016-10-11 11:11:00 +0800

    answer to http://stackoverflow.com/questions/39935846/displaying-a-different-skscene-swift

---
+++ SceneTest/SceneTest/GameViewController.swift
@@ -0,0 +1,71 @@
+//
+//  GameViewController.swift
+//  SceneTest
+//
+//  Created by 晨韦 on 16/10/11.
+//  Copyright © 2016年 晨韦. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+//            if let scene = SKScene(fileNamed: "GameScene") {
+//                // Set the scale mode to scale to fit the window
+//                scene.scaleMode = .aspectFill
+//                
+//                // Present the scene
+//                view.presentScene(scene)
+//            }
+            // Load the SkScene from self defined 'MyScene.sks'
+            if let scene = SKScene(fileNamed: "MyScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            // Load the SKScenef from self defined YourScene
+//            let scene = YourScene(size: view.bounds.size)
+//            /* Set the scale mode to scale to fit the window */
+//            scene.scaleMode = .resizeFill
+//            
+//            view.presentScene(scene)
+
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
---
+++ SceneTest/SceneTest/YourScene.swift
@@ -0,0 +1,76 @@
+//
+//  YourScene.swift
+//  SceneTest
+//
+//  Created by 晨韦 on 16/10/11.
+//  Copyright © 2016年 晨韦. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class YourScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+    
+        self.backgroundColor = UIColor.yellow
+        let hint = SKLabelNode(text: "Welcome to Your Scene")
+        hint.fontColor = UIColor.black
+        hint.fontSize = 36
+        hint.position = CGPoint(x: frame.midX, y: frame.midY)
+       self.addChild(hint)
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.green
+            self.addChild(n)
+        }
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.blue
+            self.addChild(n)
+        }
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.red
+            self.addChild(n)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let label = self.label {
+            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        }
+        
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
